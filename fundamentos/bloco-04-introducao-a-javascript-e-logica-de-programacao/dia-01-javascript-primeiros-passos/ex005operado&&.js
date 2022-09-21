88924cd83e4f5ff864aa7e98cf6c4b055d9f106d
alexnunesc <alexnunescdev@gmail.com>
2022-09-21 15:51:59 -0300

    operador OR ||

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/ex005operado&&.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/ex005operado&&.js
@@ -8,9 +8,9 @@ if (currentHour >= 22) {
   msg = 'Rango da noite, vamos jantar :D';
 } else if (currentHour >= 14 && currentHour < 18) {
   msg = 'Vamos fazer um bolo pro café da tarde?';
-} else if (currentHour > 11 && currentHour < 14) {
+} else if (currentHour >= 11 && currentHour < 14) {
   msg = 'Hora do almoço!!!';
-} else if (currentHour >= 4 && currentHour <= 11 ) {
+} else if (currentHour >= 4 && currentHour < 11 ) {
   msg = 'Hmmm, cheiro de café recém-passado';
 }
 
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts-dos-videos/009operadorOr.js
@@ -0,0 +1,4 @@
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
