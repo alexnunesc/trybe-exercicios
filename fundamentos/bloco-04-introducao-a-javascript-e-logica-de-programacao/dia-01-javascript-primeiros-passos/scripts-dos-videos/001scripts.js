987ba84f5e80754fa1ffcad375c153d32c01496a
alexnunesc <alexnunescdev@gmail.com>
2022-09-21 09:11:08 -0300

    adicioonando js

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts-dos-videos/001scripts.js
@@ -0,0 +1,6 @@
+let a = 'alex'
+const n = 20;
+
+console.log(a)
+
+console.log(n)
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts-dos-videos/002script.js
@@ -0,0 +1,28 @@
+let movie = "Avengers"; //string literal
+
+let score = 110; //number literal
+
+let isValid = true; //boolean
+
+let name; //undefined indefinida
+
+let color = null; //útil para o usuário mundar o vlaor do site.
+
+//operações aritméticas.
+// + - * / **
+let salary = 3500;
+console.log(salary+2);
+console.log(salary-2);
+console.log(salary*2);
+console.log(salary/2);
+console.log(salary**2);
+
+/*
+n ++ pos incrementor
+n --
+
+(Significa  N = n + 1)
+*/
+salary++,
+console.log(salary);
+
