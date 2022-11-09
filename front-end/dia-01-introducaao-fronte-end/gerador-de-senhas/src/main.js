72dffc89d55b20c527487178b9c40e33e8c218a6
alexnunesc <alexnunescdev@gmail.com>
2022-11-09 13:25:54 -0300

    day one

--- front-end/dia-01-introducaao-fronte-end/gerador-de-senhas/src/main.js
+++ front-end/dia-01-introducaao-fronte-end/gerador-de-senhas/src/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import { nanoid } from "nanoid";
+import "./style.css";
+import copy from "clipboard-copy";
+//console.log('Hello World');
+
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => displayPasswordEl.innerHTML = nanoid());
+
+displayPasswordEl.addEventListener('click', (event) => {
+  copy(event.target.innerText)
+});
