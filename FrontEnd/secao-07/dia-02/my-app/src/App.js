3c809b4aa40f2ef3d8a2c9ddc495a0087aa88bdb
alexnunesc <alexnunescdev@gmail.com>
2023-01-26 15:07:31 -0300

    configurando redux no react

---
+++ FrontEnd/secao-07/dia-02/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Contador</h1>
+      <h2>0</h2>
+      <button type="button">Incrementar</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/secao-07/dia-02/my-app/src/redux/index.js
@@ -0,0 +1,11 @@
+// ./src/redux/index.js
+import {legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const INITIAL_STATE = { count: 0};
+
+const reducer = (state = INITIAL_STATE, action) => state;
+
+const store = createStore(reducer, composeWithDevTools());
+
+export default store;
